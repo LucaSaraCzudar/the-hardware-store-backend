f77580bcde49689dc01aefc012d5c3347c7d3034
Luca Sara Czudar <czudar.luca@gmail.com>
2022-05-25 11:27:25 +0200

    Add configuration

--- src/main/java/hardwarestore/HardwareStoreApplication.java
+++ src/main/java/hardwarestore/HardwareStoreApplication.java
@@ -2,20 +2,13 @@ package hardwarestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
-public class HardwareStoreApplication implements WebMvcConfigurer {
+public class HardwareStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HardwareStoreApplication.class, args);
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*");
 	}
 }
